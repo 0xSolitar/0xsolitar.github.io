619f644e761d75eb6c9922f905c74062530cc87c
Solitär <clermont.judicael@proton.me>
2026-04-03 11:37:43 +0300

    update

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,17 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const posts = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    published: z.date(),
-    description: z.string().optional(),
-    image: z.string().optional(),
-    tags: z.array(z.string()).default([]),
-    category: z.string(),
-    draft: z.boolean().default(false),
-  }),
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      published: z.date(),
+      description: z.string().optional(),
+      image: image().optional(),
+      tags: z.array(z.string()).default([]),
+      category: z.string(),
+      draft: z.boolean().default(false),
+    }),
 });
 
 export const collections = { posts };
